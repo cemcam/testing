e3889587e9280893899d5ab901b640c9624ba17e
Cem Camur <s339026@oslomet.no>
2021-01-29 19:24:43 +0100

    TEST!!!!

--- src/test/java/oslomet/testing/EnhetstestBankController.java
+++ src/test/java/oslomet/testing/EnhetstestBankController.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-
+/////DETTE ER EN TEST!!!!!
 @RunWith(MockitoJUnitRunner.class)
 public class EnhetstestBankController {
 
